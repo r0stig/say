d71a5f25b1c214dfbd197681378e4b80c2c39b52
Robert Säll <pr_125@hotmail.com>
2014-05-12 12:56:57 +0200

    Resumes queue after message has been playing or not if sonos is not playing

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import sys, os
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../SoCo'))
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../soco-test/SoCo'))
 print path
 if not path in sys.path:
     sys.path.insert(1, path)
@@ -8,37 +8,44 @@ from soco import SoCo
 from flask import Flask
 from say import text2mp3
 import time, threading
+import soco
 from threading import Timer
+from mutagen.mp3 import MP3
 app = Flask(__name__)
 
 PATH = './static/'
 
 #Constants of your choice
 LANGUAGE = 'en' # language for speech (en = English)
-IP = "192.168.101.178"             #Sonos player to use
+IP = "192.168.1.90"             #Sonos player to use
+LAN_IP = "192.168.1.29"         # Local area ip that sonos can reach
 
 
 @app.route('/say/<text>', methods=['GET'])
 def say(text):
     ok, file_name =  text2mp3(text, PATH, LANGUAGE)
     if ok:
-        print "Created file.."
-        # Play the URL...
         zp = SoCo(IP)
         cur_info = zp.get_current_track_info()
-        zp.play_uri("http://192.168.100.123:5000/static/speech.mp3")
-        speech_info = zp.get_current_track_info()
-        duration = speech_info['duration']
-        Timer(duration, resume_queue, (zp, cur_info)).start()
+        state = zp.get_current_transport_info()
+        zp.play_uri("http://{0}:5000/static/speech.mp3".format("192.168.1.29"))
+        if (state['current_transport_state'] == 'PLAYING'):
+            audio = MP3("./static/speech.mp3")
+            speech_info = zp.get_current_track_info()
+            duration = speech_info['duration']
+            Timer(audio.info.length, resume_queue, (zp, cur_info)).start()
+    return ""
 
 def resume_queue(zp, info):
     position = info['position']
     pl_position = info['playlist_position']
-    zp.play_from_queue(pl_position)
+    zp.play_from_queue(int(pl_position) - 1 ) #Until newest version applied -1 is needed
     zp.play()
     zp.seek(position)
-    print "resumes queue"
 
 
 if __name__ == "__main__":
+   #for zone in soco.discover():
+   #     print zone
+
     app.run(host='0.0.0.0', debug=True)
